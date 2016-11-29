1bdd82e12f6123067b2b5c2a29aee1eca280e546
RyZum <ryzumm@gmail.com>
2016-11-29 14:16:43 +0100

    Quartile implementation

--- src/main.py
+++ src/main.py
@@ -2,11 +2,13 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import time
+import datetime
  
 from sklearn import linear_model
 
-data_size = 1000; #max is 1030829
-resFileName = '../submission002.txt'
+data_size = -1; #max is 1030829
+resFileName = '../submission003.txt'
+showGraphs= False;
 
 reg = linear_model.LinearRegression()
 
@@ -14,21 +16,35 @@ start_time = time.time()
 
 def computePrediction(date, assignment, data):
     weekend = (date.weekday() > 4)
-    relevant = data.loc[lambda x : (x.MONTH == date.month) & (x.WEEK_END == weekend) & (x.ASS_ASSIGNMENT == assignment) & (x.HOUR == date.hour)]
+    relevant = data.loc[lambda x : 
+                        (x.WEEK_END == weekend) & 
+                        (x.ASS_ASSIGNMENT == assignment) & 
+                        (x.HOUR == date.hour) & 
+                        (x.DATE < date + datetime.timedelta(30)) &
+                        (x.DATE > date - datetime.timedelta(30))]
     if len(relevant) > 0 :
-#         if relevant.quantile(0.9).CSPL_CALLS > 0 :
-#             print(len(relevant)) 
-#             relevant.CSPL_CALLS.plot(marker='o')
-#             print(relevant.quantile(0.8, interpolation='nearest').CSPL_CALLS)
-#             plt.show()
-#             plt.close()
-        return relevant.quantile(0.5, interpolation='higher').CSPL_CALLS
+        if showGraphs and relevant.quantile(0.9).CSPL_CALLS > 0 :
+            relevant.CSPL_CALLS.plot(marker='o')
+            print(str(len(relevant)) + " points plotted, result is : " + str(relevant.quantile(0.8, interpolation='nearest').CSPL_CALLS))
+            plt.show()
+            plt.close()
+        return relevant.quantile(0.8, interpolation='higher').CSPL_CALLS
     else :
         return 0
     
+def computeTimeString():
+    t = time.time() - start_time
+    if t < 120 :
+        return "%.2f" % (time.time() - start_time) + " sec."
+    else : 
+        return str(int(t/60)) + " min. " + str(int(t % 60)) + " sec."
+    
 def loadData() : 
-    #nrows = data_size, 
-    data = pd.read_csv('../train_2011_2012_2013.csv', parse_dates = ['DATE'], sep = ';',  engine='c', usecols=['DATE','DAY_OFF', 'WEEK_END', 'DAY_WE_DS', 'ASS_ASSIGNMENT', 'CSPL_CALLS']) # low_memory=False
+    if data_size > 0 : 
+        data = pd.read_csv('../train_2011_2012_2013.csv', nrows = data_size, parse_dates = ['DATE'], sep = ';',  engine='c', usecols=['DATE','DAY_OFF', 'WEEK_END', 'DAY_WE_DS', 'ASS_ASSIGNMENT', 'CSPL_CALLS']) # low_memory=False
+    else : 
+        data = pd.read_csv('../train_2011_2012_2013.csv', parse_dates = ['DATE'], sep = ';',  engine='c', usecols=['DATE','DAY_OFF', 'WEEK_END', 'DAY_WE_DS', 'ASS_ASSIGNMENT', 'CSPL_CALLS']) # low_memory=False
+
     data['DATE'] = pd.to_datetime(data['DATE'], unit='ms')
 
     pd.set_option('display.max_rows', len(data.iloc[0]))
@@ -43,6 +59,10 @@ def loadPredictions() :
     prediction = pd.read_table('../submission.txt', parse_dates = ['DATE'])
     return prediction
 
+###########################################
+################### CODE ##################
+###########################################
+
 data = loadData();
 prediction = loadPredictions()
 print("Training data is loaded (" + str(len(data.index)) + ")")
@@ -52,7 +72,7 @@ predictionsNb = len(prediction.index)
 
 for i in range(predictionsNb) :
     if (i % 100 == 0) :
-        print("Dealing with test case " + str(i) + "/" + str(predictionsNb) + " \t(" + "%.2f" % (100*i/predictionsNb) + "% in " + "%.2f" % (time.time() - start_time) + " sec. )")
+        print("Dealing with test case " + str(i) + "/" + str(predictionsNb) + " \t(" + "%.2f" % (100*i/predictionsNb) + "% in " + computeTimeString() + " )")
     date = prediction.iloc[i]['DATE']
     assignment = prediction.iloc[i]['ASS_ASSIGNMENT']
     res = computePrediction(date, assignment, data)
@@ -62,7 +82,7 @@ for i in range(predictionsNb) :
     
 print("Computation finished. Non zero value computed : " + str(nonZeroRes))
 
-prediction.to_csv(resFileName, sep = '\t', index = False)
+prediction.to_csv(resFileName, sep = '\t', index = False, encoding ='utf-8')
 print("Saved to " + resFileName)
 pd.reset_option('display.max_rows')
 
